037fbcee099fec7e57093d3cc6753f731465049e
MartinezHrcz <160034549+MartinezHrcz@users.noreply.github.com>
2025-02-03 19:27:33 +0100

    JavaFX UI implemente

--- src/main/java/UI/StartUI.java
+++ src/main/java/UI/StartUI.java
@@ -13,7 +13,7 @@ public class StartUI extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartUI.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Expense App!");
+        stage.setTitle("ExpenseApp!");
         stage.setScene(scene);
         stage.show();
     }
