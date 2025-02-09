013eeb17d20899cf74b8c9305666d4cfd96b05b7
MartinezHrcz <160034549+MartinezHrcz@users.noreply.github.com>
2025-02-09 23:05:31 +0100

    UI Changes Added: -App icon (appicon.png) -HomeController change: switch case chooses which fxml file will be loaded into the stage -StartUI change: new menu selection doesn't open a new window for the change -UI: window size is now locked, in the future i will make it resizable and dynamic

--- src/main/java/UI/HomeController.java
+++ src/main/java/UI/HomeController.java
@@ -1,11 +1,24 @@
 package UI;
 
+import java.awt.*;
 import java.io.IOException;
-
+import javafx.scene.control.Button;
 public class HomeController {
 
 
     public void ButtonClicked(javafx.event.ActionEvent actionEvent) throws IOException {
-        UI.StartUI.IncomeLoad();
+        Button bt = (Button) actionEvent.getSource();
+        switch (bt.getId().toString()){
+            case "Income_Btn":
+                UI.StartUI.IncomeLoad();
+                break;
+            case "Expenses_Btn":
+                UI.StartUI.ExpenseLoad();
+                break;
+            case "Finances_Btn":
+                UI.StartUI.FinancesLoad();
+                break;
+        }
+
     }
 }
