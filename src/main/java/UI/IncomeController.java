a6165af9ca53cc96f10d3729803093c567f73ffe
MartinezHrcz <160034549+MartinezHrcz@users.noreply.github.com>
2025-02-11 19:12:31 +0100

    UI Changes Added: -App icon (appicon.png) -HomeController change: switch case chooses which fxml file will be loaded into the stage -StartUI change: new menu selection doesn't open a new window for the change -UI: window size is now locked, in the future i will make it resizable and dynamic

--- src/main/java/UI/IncomeController.java
+++ src/main/java/UI/IncomeController.java
@@ -1,5 +1,14 @@
 package UI;
 
+import javafx.event.ActionEvent;
+
 public class IncomeController {
 
+    public void AddIncomeMenu(ActionEvent actionEvent) {
+
+    }
+
+    public void AddMenuLoad(){
+
+    }
 }
