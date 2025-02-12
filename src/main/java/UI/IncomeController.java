9425fbf6bedb2a4a867b92452e324ff67e998aac
MartinezHrcz <160034549+MartinezHrcz@users.noreply.github.com>
2025-02-12 17:41:42 +0100

    Started making the functions for Adding income -IncomeAdder menu uses IncomeController.java

--- src/main/java/UI/IncomeController.java
+++ src/main/java/UI/IncomeController.java
@@ -1,13 +1,31 @@
 package UI;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TextInputControl;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class IncomeController {
+
+    @FXML
+    private TextField Name;
+    @FXML
+    private TextField Amount;
+    @FXML
+    private TextField Description;
+    @FXML
+    private TextField Date;
+
     public void AddIncomeMenu(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(IncomeController.class.getResource("IncomeMenuSub/IncomeAdder.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -29,5 +47,10 @@ public class IncomeController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void AddIncomePress(ActionEvent actionEvent) {
+        Scene p = StartUI.window.getScene();
+
     }
 }
