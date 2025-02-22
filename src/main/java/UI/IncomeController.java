1f0d15a0b92949de22aa6b1ce46bdb5b3853eb79
MartinezHrcz <160034549+MartinezHrcz@users.noreply.github.com>
2025-02-22 11:04:03 +0100

    Accident fixed -Deleted "Items="all"" property from income fxmls ListView node

--- src/main/java/UI/IncomeController.java
+++ src/main/java/UI/IncomeController.java
@@ -5,6 +5,7 @@ import Application.Classes.ListIncomeExpenses;
 import javafx.collections.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -12,11 +13,13 @@ import javafx.scene.text.Text;
 
 import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 
-public class IncomeController{
+public class IncomeController implements Initializable {
     @FXML
     public ListView<String> Income_List;
     //Boxes on the sceen
@@ -46,12 +49,18 @@ public class IncomeController{
         ListIncomeExpenses.IncomeList.forEach(x-> {
             list.add(x.toString());
         });
-        ObservableList<String> observableList = FXCollections.observableList(list);
+        //  ObservableList<String> observableList = FXCollections.observableList(list);
         //ObservableList<String> observableList2 = FXCollections.observableList();
-        Income_List.setItems(FXCollections.observableArrayList( "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise"));
+       // Income_List.setItems(FXCollections.observableArrayList( "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise"));
     }
 
     public void DeleteIncomeMenu() {
 
+    }
+
+    String[] xyz = {"ds","dsf",""};
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       // Income_List.getItems().setAll(xyz);
     }
 }
