b4e7d6c01c1767cefe03383cd112bd961a84c5b6
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-03-04 13:36:15 +0100

    Trying new commit

--- src/main/java/Application/Interfaces/GeneralListFunctions.java
+++ src/main/java/Application/Interfaces/GeneralListFunctions.java
@@ -4,4 +4,5 @@ public interface GeneralListFunctions {
     boolean AddToList(String name, double amount, String desc, int Date,  boolean ess, boolean checked);
     boolean RemoveFromList();
     static double Sum(){double sum = 0; return sum;};
+
 }
